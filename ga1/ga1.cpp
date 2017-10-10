93c0254eafb55497bf16e1fc5b5d330bd14b8b4a
Peter Dorich <dorichp@flip3.engr.oregonstate.edu>
2017-10-10 16:57:32 -0700

    Added base case and merge sort functions

--- ga1/ga1.cpp
+++ ga1/ga1.cpp
@@ -164,16 +164,81 @@ int binSearch(ifstream &myFile, int start, int end, long x){
   }
 
 }
+//Regular MergeSort algorithm
+void merge(int arr[], int l, int m, int r)
+{
 
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	
+	int L[n1], R[n2];
+
+	for(i = 0; i <n1; i++)
+		L[i] = arr[l + i];
+	for(j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+	i = 0;
+	j = 0;
+	k = l;
+	
+	while( i < n1 && j < n2)
+	{
+		if (L[i] < = R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while( j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+//l = left index and r = right index of sub-array
+void mergeSort(int arr[], int l, int r)
+{
+	if(l < r)
+	{
+		int m = l+(r-l)/2;
+	
+	mergeSort(arr, l, m);
+	mergeSort(arr, m+1, r);
+	
+	merge(arr, l, m, r);
+	}
+
+}
 
 int kthSmallest(int m, int n, int k, int *beginning, int *end, ifstream *myFiles){
    int longest, midIndex, nums;
    long mid;
-   //if n of all .dat files == 1
-      //combine into 1 array, sort, and return kth element
 
-   ///else
+	int tmp_array[m];	//array that will hold all first values from m arrays
+	if( n <= 1)	//if the array size is only 1 or zero, combine into 1 array and sort. 
+	{
+		for( int i = 0; i < m; i ++)
+		{
+			tmp_array[i] = getFromFile(myFiles[i], 0);
+		}
+		
+	//tmp_array now holds all first values 
+	//merge sort:
+	int p = m/sizeof(myFiles[0]);
+	mergeSort(myFiles, 0, m - 1);
 
+
+
+	}
       //find longest "working" .dat length
       longest = findLongest(myFiles, beginning, end, m);    //store index of which .dat file into longest
       //retrieve the median index
